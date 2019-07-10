fdd49733e43a499d94f895d7adabe1c04e2c49d7
sidzioor <rpaczkowski98@gmail.com>
2019-07-10 17:37:24 +0200

    Add packets: user, calculator and tests them

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/user/SimplyUser.java
@@ -0,0 +1,13 @@
+package com.kodilla.testing.user;
+
+public class SimplyUser {
+    private  String username;
+
+    public SimplyUser(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}

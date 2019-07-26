7c74fbf20c05b69a5cbcf8d4c88da1648de3a42d
sidzioor <rpaczkowski98@gmail.com>
2019-07-26 12:37:41 +0200

    Change ArrayOperations and write with orElse method

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,18 +1,16 @@
 package com.kodilla.stream.array;
 
+import java.util.Arrays;
+import java.util.OptionalDouble;
 import java.util.stream.IntStream;
 
 public interface ArrayOperations {
     static double getAverage(int[] numbers) {
-        if (numbers.length > 0) {
-            IntStream numbersForAverage = IntStream.range(0, numbers.length);
-            System.out.println(numbersForAverage);
-            double numberAverage = IntStream.range(0, numbers.length)
-                    .map(n -> numbers[n])
-                    .average().getAsDouble();
-            return numberAverage;
-        } else {
-            return 0.000;
-        }
+        IntStream numbersForAverage = IntStream.range(0, numbers.length);
+        System.out.println(numbersForAverage);
+        OptionalDouble numberAverage = Arrays.stream(numbers)
+                .average();
+
+       return numberAverage.orElse(0);
     }
 }
